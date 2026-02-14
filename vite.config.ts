d66754702d31dd38f6cb7da5c769aec92f27a42a
sagartiwari3136-cyber <sagartiwari3136@gmail.com>
2026-02-15 00:29:50 +0530

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/For-You-Always-main/",
+  base: "/trial/",
   plugins: [react()],
 
   root: path.resolve(__dirname, "client"),
